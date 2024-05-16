fc905353ac9eca438e8dd39b4bf0b6385704a63a
Emilio Bensignor <liobensignor@gmail.com>
2024-05-16 15:16:41 -0300

    Notas de Prensa

---
+++ content/js/notas-de-prensa.js
@@ -0,0 +1,24 @@
+$(document).ready(function () {
+  $('.sliderBanner').slick({
+    infinite: true,
+    dots: true,
+    speed: 600,
+    arrows: false,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+  });
+});
+$(document).ready(function () {
+  $('.sliderBannerDesktop').slick({
+    infinite: true,
+    dots: true,
+    speed: 600,
+    arrows: false,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+  });
+});
